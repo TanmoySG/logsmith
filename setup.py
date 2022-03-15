65af89abcf6033f9fe1c92b4f4f9e1a78a98f2b9
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-03-15 15:39:22 +0000

    Published v0.0.3 with Dependancies Installation Fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='logsmith',
-    version="0.0.2",
+    version="0.0.3",
     author='Tanmoy Sen Gupta',
     author_email='tanmoysps@gmail.com',
     url='https://github.com/TanmoySG/logsmith',
