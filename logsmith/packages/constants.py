5d4fee994e2cc2cb52f881c873cf80244a8ab5bf
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-06-18 07:16:35 +0000

    Renamed handlers to packages #5

--- logsmith/packages/constants.py
+++ logsmith/packages/constants.py
@@ -2,6 +2,19 @@ from types import SimpleNamespace
 
 DefaultLogStatementPattern = "[{timestamp}] {message}"
 
+
+
+LogLevels = SimpleNamespace(
+    **{
+        "WARN": "WARN",
+        "INFO": "INFO",
+        "CRITICAL": "CRITICAL",
+        "SUCCESS": "SUCCESS",
+        "FAILURE": "FAILURE",
+        "CUSTOM": "CUSTOM"
+    }
+)
+
 LogFormats = SimpleNamespace(
     **{
         "JSON": "JSON",
