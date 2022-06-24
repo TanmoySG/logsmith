608e2c61ca77ee3ef74c77e7ba3e1603aea0dd66
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-06-24 18:23:13 +0000

    Fixed Formatter

--- logsmith/packages/monitor.py
+++ logsmith/packages/monitor.py
@@ -3,58 +3,72 @@ import os
 import requests
 from logsmith.packages.utilities import String
 
+
 class Endpoints:
-    API= String.Template("{0}/")
-    Publisher= String.Template("{0}/publisher")
-    checkPublisher=String.Template("{0}/{1}")
-    Context= String.Template("{0}/{1}/context")
-    checkContext= String.Template("{0}/{1}/{2}")
-    Log= String.Template("{0}/{1}/{2}/logs")
+    API = String.Template("{0}/")
+    Publisher = String.Template("{0}/publisher")
+    checkPublisher = String.Template("{0}/{1}")
+    Context = String.Template("{0}/{1}/context")
+    checkContext = String.Template("{0}/{1}/{2}")
+    Log = String.Template("{0}/{1}/{2}/logs")
 
 
-URITemplate =  String.Template("{0}://{1}:{2}")
+URITemplate = String.Template("{0}://{1}:{2}")
+
 
 class DefaultPublisherTemplate:
     Origin = String.Template("app.{0}.com")
     Description = String.Template("Logs Published by {0}")
-    
+
+
 class DefaultContextTemplate:
     Origin = String.Template("app.{0}.com/{1}")
     Description = String.Template("Log Published by {0} to context {1}")
-    Kind = {
-        "logs": []
-    }
+    Kind = {"logs": []}
+
 
 class RequestHeaders:
-    POST = { 'Content-Type': 'application/json' }
+    POST = {"Content-Type": "application/json"}
+
 
 class Monitor:
     def __init__(self, monitorConfig=None) -> None:
-        """
-
-        """
+        """ """
         self.config = monitorConfig
         pass
 
-    def check(self):
+    def check(self) -> dict:
         endpoint = Endpoints.API.fill(data=[self.config["monitorListener"]])
         r = requests.get(url=endpoint)
-        print(r.json())
+        return r.json()
 
-    
     def initiate(self):
         pass
 
     def getConfigs(self, config) -> dict:
         monitorConfigs = {}
         if "monitor" not in config:
-            return  monitorConfigs 
+            return monitorConfigs
         monitorConfigs["monitorPort"] = config["monitor"]["port"]
         monitorConfigs["monitorURI"] = config["monitor"].get("server")
-        monitorConfigs["monitorProtocol"] = config["monitor"].get("protocol" , "http")
-        monitorConfigs["monitorListener"] =  os.environ.get("LISTENER" , URITemplate.fill([monitorConfigs["monitorProtocol"], monitorConfigs["monitorURI"], monitorConfigs["monitorPort"]]))
-        monitorConfigs["publisher"] = self.Publisher(config["monitor"]["publisher"]).getConfig()
-        monitorConfigs["context"] = self.Context(publisher=monitorConfigs["publisher"]["publisher"], contextConfig=config["monitor"]["context"]).getConfig()
+        monitorConfigs["monitorProtocol"] = config["monitor"].get("protocol", "http")
+        monitorConfigs["monitorListener"] = os.environ.get(
+            "LISTENER",
+            URITemplate.fill(
+                [
+                    monitorConfigs["monitorProtocol"],
+                    monitorConfigs["monitorURI"],
+                    monitorConfigs["monitorPort"],
+                ]
+            ),
+        )
+        monitorConfigs["publisher"] = self.Publisher(
+            config["monitor"]["publisher"]
+        ).getConfig()
+        monitorConfigs["context"] = self.Context(
+            publisher=monitorConfigs["publisher"]["publisher"],
+            contextConfig=config["monitor"]["context"],
+        ).getConfig()
         return monitorConfigs
 
     def log(self):
@@ -75,12 +89,17 @@ class Monitor:
         def getConfig(self):
             publisherConfig = {}
             publisherConfig["publisher"] = self.configs["publisher"]
-            publisherConfig["origin"] = self.configs.get("origin", DefaultPublisherTemplate.Origin.fill(data=[self.publisher]))
-            publisherConfig["description"] = self.configs.get("description",  DefaultPublisherTemplate.Description.fill(data=[self.publisher]))
+            publisherConfig["origin"] = self.configs.get(
+                "origin", DefaultPublisherTemplate.Origin.fill(data=[self.publisher])
+            )
+            publisherConfig["description"] = self.configs.get(
+                "description",
+                DefaultPublisherTemplate.Description.fill(data=[self.publisher]),
+            )
             return publisherConfig
 
-    class Context():
-        def __init__(self, publisher="default", contextConfig=None ) -> None:
+    class Context:
+        def __init__(self, publisher="default", contextConfig=None) -> None:
             self.configs = contextConfig
             self.publisher = publisher
             self.context = self.configs["context"]
@@ -95,7 +114,17 @@ class Monitor:
         def getConfig(self):
             contextConfig = {}
             contextConfig["context"] = self.configs["context"]
-            contextConfig["origin"] = self.configs.get("origin", DefaultContextTemplate.Origin.fill(data=[self.publisher, self.context]))
-            contextConfig["description"] = self.configs.get("description", DefaultPublisherTemplate.Description.fill(data=[self.publisher, self.context]))
-            contextConfig["kind"] = self.configs.get("kind", DefaultContextTemplate.Kind)
+            contextConfig["origin"] = self.configs.get(
+                "origin",
+                DefaultContextTemplate.Origin.fill(data=[self.publisher, self.context]),
+            )
+            contextConfig["description"] = self.configs.get(
+                "description",
+                DefaultPublisherTemplate.Description.fill(
+                    data=[self.publisher, self.context]
+                ),
+            )
+            contextConfig["kind"] = self.configs.get(
+                "kind", DefaultContextTemplate.Kind
+            )
             return contextConfig
